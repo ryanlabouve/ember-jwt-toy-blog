9531e32b187b8b61d86144e289c3183f17fd1ee7
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-17 12:06:14 -0500

    Add model test for public post

--- tests/unit/models/public-post-test.js
+++ tests/unit/models/public-post-test.js
@@ -5,8 +5,26 @@ moduleForModel('public-post', 'Unit | Model | public post', {
   needs: []
 });
 
-test('it exists', function(assert) {
+const expectedKeys = [
+  'title',
+  'body',
+  'createdAt'
+];
+
+test('it has the right keys', function(assert) {
   let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+  const modelKeys = Object.keys(model.toJSON())
+  modelKeys.forEach(function(key) {
+    assert.equal(
+      expectedKeys.indexOf(key) > -1,
+      true,
+      `public post should have a ${key} property`
+    );
+  });
+
+  assert.equal(
+    expectedKeys.length,
+    modelKeys.length,
+    `We are expecting ${expectedKeys.lenght} props and we have ${modelKeys.length} on the model`
+  );
 });
